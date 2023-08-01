cbda79a64683dbac188083397c3622fe70f5e3c5
SamuelDuran <samgarcia04@hotmail.com>
2023-08-01 11:43:42 -0500

    En el .gitignore agregué el venv y el .idea porque no lo tenía.
    En el ejercicio_1.py realicé el ajercicio

---
+++ ejercicioderepaso/ejercicio_1.py
@@ -0,0 +1,8 @@
+#Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla#
+
+nombre = input("ingrese su nombre: ")
+
+edad = int(input("Ingrese su edad: "))
+
+print("Su nombre es", nombre)
+print("Su edad es", edad)
