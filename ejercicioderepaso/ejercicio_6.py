f4530b68ea71aa62c89ce3849d59e95379986310
SamGa <samgarcia04@hotmail.com>
2023-08-01 23:36:05 -0500

    6. Crear un programa que calcule la suma de los números en una lista dada.

---
+++ ejercicioderepaso/ejercicio_6.py
@@ -0,0 +1,7 @@
+def calcular_suma_lista(lista):
+    suma = sum(lista)
+    return suma
+
+lista_numeros = [1, 2, 3, 4, 5]
+suma_total = calcular_suma_lista(lista_numeros)
+print("La suma de los números en la lista es:", suma_total)
